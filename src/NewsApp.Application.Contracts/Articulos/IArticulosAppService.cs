106e1aa321b9886323fe070a25112a217397e49d
Salva <salvatigua@gmail.com>
2023-11-01 16:33:43 -0300

    get_articulos_busqueda

--- src/NewsApp.Application.Contracts/Articulos/IArticulosAppService.cs
+++ src/NewsApp.Application.Contracts/Articulos/IArticulosAppService.cs
@@ -5,12 +5,13 @@ using System.Text;
 using System.Threading.Tasks;
 using Volo.Abp.Application.Services;
 using NewsApp.Articulos;
+using System.Net.NetworkInformation;
 
 namespace NewsApp.Articulos
 {
     public interface IArticulosAppService : IApplicationService
     {
         string GetArticulos(string cadena, CodigosIdiomas? idioma, OrdenBusqueda? ordenarPor);
-        public ICollection<ArticuloDto> GetArticulosNuestros(string cadena, CodigosIdiomas? idioma, OrdenBusqueda? ordenarPor);
+        public ICollection<ArticuloDto> GetArticulosNuestros(string cadena, CodigosIdiomas? idioma, OrdenBusqueda? ordenarPor, string? urls);
     }
 }
--- src/NewsApp.Application/Articulos/ArticulosAppService.cs
+++ src/NewsApp.Application/Articulos/ArticulosAppService.cs
@@ -5,6 +5,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NewsApp.Articulos;
+using AutoMapper;
+using System.Text.Json;
+using System.Collections.ObjectModel;
 
 namespace NewsApp.Articulos
 {
@@ -17,9 +20,53 @@ namespace NewsApp.Articulos
             return articulos;
         }
 
-        public ICollection<ArticuloDto> GetArticulosNuestros(string cadena, CodigosIdiomas? idioma, OrdenBusqueda? ordenarPor)
+        public ICollection<ArticuloDto> GetArticulosNuestros(string cadena, CodigosIdiomas? idioma, OrdenBusqueda? ordenarPor, string? urls)
         {
+            // Llama a GetArticulos para obtener el JSON de artículos
+            string jsonArticulos = GetArticulos(cadena, idioma, ordenarPor);
+
+            // Deserializa el JSON en una lista de objetos ArticuloJson
+            var articulosJson = JsonSerializer.Deserialize<List<Articulo>>(jsonArticulos);
+
+            if (urls != null)
+            {
+                var conjuntoUrls = SepararYAgregarAHashSet(urls);
+
+                MarcarArticulosComoVistos(conjuntoUrls, articulosJson); // Marca los artículos como "Vistos"
+            }
+
+            // Mapea los objetos ArticuloJson a ArticuloDto utilizando AutoMapper
+            var articulosDto = ObjectMapper.Map<ICollection<Articulo>, ICollection<ArticuloDto>>(articulosJson);
+            return articulosDto;
 
         }
+
+        public void MarcarArticulosComoVistos(ICollection<string> urls, ICollection<Articulo> articulos)
+        {
+            foreach (var articulo in articulos)
+            {
+                if (urls.Contains(articulo.Url))
+                {
+                    articulo.Visto = true;
+                }
+            }
+        }
+
+        public static HashSet<string> SepararYAgregarAHashSet(string input)
+        {
+            HashSet<string> conjunto = new HashSet<string>();
+
+            // Dividir la cadena en elementos usando comas como delimitadores
+            string[] elementos = input.Split(',');
+
+            foreach (string elemento in elementos)
+            {
+                // Agregar cada elemento al conjunto
+                conjunto.Add(elemento);
+            }
+
+            return conjunto;
+        }
+
     }
 }
