c74adb3cd28e1ccd4bf947127adf6364f5b9ab2c
Salva <salvatigua@gmail.com>
2023-11-24 09:34:24 -0300

    corrección de Tests, ahora funcionan todos

--- test/NewsApp.TestBase/NewsAppTestDataSeedContributor.cs
+++ test/NewsApp.TestBase/NewsAppTestDataSeedContributor.cs
@@ -12,22 +12,26 @@ public class NewsAppTestDataSeedContributor : IDataSeedContributor, ITransientDe
 {
     private readonly IListaRepository _listaRepository;
     private readonly IdentityUserManager _identityUserManager;
-    public NewsAppTestDataSeedContributor(IListaRepository listaRepository, IdentityUserManager identityUserManager)
+    private readonly IIdentityUserRepository _identityUserRepository;
+    public NewsAppTestDataSeedContributor(IListaRepository listaRepository, IdentityUserManager identityUserManager, IIdentityUserRepository identityUserRepository)
     {
         _listaRepository = listaRepository;
         _identityUserManager = identityUserManager;
+        _identityUserRepository = identityUserRepository;
     }
     public async Task SeedAsync(DataSeedContext context)
     {
 
         // Añadir usuarios
-        IdentityUser identityUser1 = new IdentityUser(Guid.Parse("2e701e62-0953-4dd3-910b-dc6cc93ccb0d"), "admin", "admin@abp.io");
-        await _identityUserManager.CreateAsync(identityUser1, "1q2w3E*");
+        IdentityUser identityUser = new IdentityUser(Guid.Parse("2e701e62-0953-4dd3-910b-dc6cc93ccb0d"), "admin", "admin@abp.io");
+        await _identityUserManager.CreateAsync(identityUser, "1q2w3E*");
+
+        var user = await _identityUserRepository.InsertAsync(identityUser);
 
         // Añadir 
-        await _listaRepository.InsertAsync(new Lista {Nombre = "Primera Lista", Descripcion= "Descripción de la primera lista", UsuarioId = identityUser1.Id, Alerta=false, FechaCreacion=DateTime.Today});
-        await _listaRepository.InsertAsync(new Lista { Nombre = "Segunda Lista", Descripcion = "Descripción de la segunda lista", UsuarioId = identityUser1.Id, Alerta = false, FechaCreacion = DateTime.Today});
-        await _listaRepository.InsertAsync(new Lista { Nombre = "Tercera Lista", Descripcion = "Descripción de la tercera lista", UsuarioId = identityUser1.Id, Alerta = false, FechaCreacion = DateTime.Today});
+        await _listaRepository.InsertAsync(new Lista {Nombre = "Primera Lista", Descripcion= "Descripción de la primera lista", UsuarioId = identityUser.Id, Alerta=false, FechaCreacion=DateTime.Today});
+        await _listaRepository.InsertAsync(new Lista { Nombre = "Segunda Lista", Descripcion = "Descripción de la segunda lista", UsuarioId = identityUser.Id, Alerta = false, FechaCreacion = DateTime.Today});
+        await _listaRepository.InsertAsync(new Lista { Nombre = "Tercera Lista", Descripcion = "Descripción de la tercera lista", UsuarioId = identityUser.Id, Alerta = false, FechaCreacion = DateTime.Today});
         
     }
 }
